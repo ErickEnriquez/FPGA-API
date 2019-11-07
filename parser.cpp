2f4f31a4290fee1322434a432e8912949be1eee1
khanmi97 <mikhan3@asu.edu>
2019-11-07 04:01:03 -0700

    very basic command added; not a parser

--- parser.cpp
+++ parser.cpp
@@ -4,8 +4,50 @@
 
 using namespace std;
 
+int main()
+{
+    string command, file;
 
-int main(int argc, char** argv){
+    cout << "The following commands can be passed to the command-line interface:" << endl;
+    cout << "To transfer file from PC to FPGA card: transfer 'file name'" << endl;
+    cout << "To access files in the DMA: read 'file name'" << endl;
+    cout << "To delete a file: delete 'file name'" << endl;
+
+    cout << endl << "Enter your command" << endl;
+    cin >> command;
+    getline(cin, file);
+
+    if (command.compare("transfer") == 0)
+    {
+
+        /**Code for the operations goes here**/
+
+        // if file does not exist
+        cout << "file not found" << endl;
+    }
+    else if (command.compare("read") == 0) // opens a file for reading
+    {
+        /**Code for the operations goes here**/
+
+        cout << "file not found" << endl;
+    }
+    else if (command.compare("delete") == 0)
+    {
+        /**Code for the operations goes here**/
+
+        cout << "file not fount" << endl;
+    }
+    else
+    {
+        cout << "invalid command!" << endl;
+    }
+
+
+
+    return 0;
+}
+
+/*int main(int argc, char** argv){
     cout<<"NUMBER OF ARG = " << argc <<"\n";
     for(int i = 1; i < argc ;i++){
         cout<< argv[i] << "\n";
@@ -13,4 +55,4 @@ int main(int argc, char** argv){
     cout<<"TESTING\n";
 
     return 0;
-}
+}*?
