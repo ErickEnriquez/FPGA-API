8cd0577f412aeb10cb47102a1d159659b50abfd6
Erick Enriquez <eenriqu5@asu.edu>
2019-12-04 13:52:13 -0700

    up to date with master branch
    merged the master branch back with my branch so they are in sync

--- parser.cpp
+++ parser.cpp
@@ -22,14 +22,6 @@ int main(int  argc , char ** argv){
     else
         parser(argc,argv);
 
-    FILE * fp;
-
-    fp = fopen(argv[1],"rb");//open file to read
-    if(fp == NULL)
-        cout<<"Error opening file";
-    
-    char* peripheral  = argv[2];//get the peripheral that we want to send to 
-    
     return 0;
 }
 
