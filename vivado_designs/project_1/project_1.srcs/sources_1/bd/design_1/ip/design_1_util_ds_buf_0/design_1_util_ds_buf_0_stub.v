b9b6d0b79ddac855f9ac4a90710428e249a45ce1
Luke Shaffer <lukeshaffer123@gmail.com>
2019-12-05 22:57:03 -0700

    Initial DMA design

---
+++ vivado_designs/project_1/project_1.srcs/sources_1/bd/design_1/ip/design_1_util_ds_buf_0/design_1_util_ds_buf_0_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (lin64) Build 2700185 Thu Oct 24 18:45:48 MDT 2019
+// Date        : Thu Nov  7 20:20:58 2019
+// Host        : desktop running 64-bit Ubuntu 18.04.2 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/luke/Documents/Capstone/Vivado_Projects/project_1/project_1.srcs/sources_1/bd/design_1/ip/design_1_util_ds_buf_0/design_1_util_ds_buf_0_stub.v
+// Design      : design_1_util_ds_buf_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xcku040-ffva1156-2-e
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "util_ds_buf,Vivado 2019.2" *)
+module design_1_util_ds_buf_0(IBUF_DS_P, IBUF_DS_N, IBUF_OUT, IBUF_DS_ODIV2)
+/* synthesis syn_black_box black_box_pad_pin="IBUF_DS_P[0:0],IBUF_DS_N[0:0],IBUF_OUT[0:0],IBUF_DS_ODIV2[0:0]" */;
+  input [0:0]IBUF_DS_P;
+  input [0:0]IBUF_DS_N;
+  output [0:0]IBUF_OUT;
+  output [0:0]IBUF_DS_ODIV2;
+endmodule
