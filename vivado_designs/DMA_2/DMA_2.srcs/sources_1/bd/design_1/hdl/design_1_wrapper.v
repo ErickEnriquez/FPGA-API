cdee19c9e79868b1e265b627455710dad7d5dfe5
Luke Shaffer <lukeshaffer123@gmail.com>
2020-01-06 23:49:05 -0700

    Simpler design with working DMA transfers

---
+++ vivado_designs/DMA_2/DMA_2.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,112 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.2 (lin64) Build 2700185 Thu Oct 24 18:45:48 MDT 2019
+//Date        : Mon Jan  6 15:47:03 2020
+//Host        : desktop running 64-bit Ubuntu 18.04.2 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (ddr4_sdram_act_n,
+    ddr4_sdram_adr,
+    ddr4_sdram_ba,
+    ddr4_sdram_bg,
+    ddr4_sdram_ck_c,
+    ddr4_sdram_ck_t,
+    ddr4_sdram_cke,
+    ddr4_sdram_cs_n,
+    ddr4_sdram_dm_n,
+    ddr4_sdram_dq,
+    ddr4_sdram_dqs_c,
+    ddr4_sdram_dqs_t,
+    ddr4_sdram_odt,
+    ddr4_sdram_reset_n,
+    default_sysclk_300_clk_n,
+    default_sysclk_300_clk_p,
+    pci_express_x8_rxn,
+    pci_express_x8_rxp,
+    pci_express_x8_txn,
+    pci_express_x8_txp,
+    pcie_perstn,
+    pcie_refclk_clk_n,
+    pcie_refclk_clk_p,
+    reset);
+  output ddr4_sdram_act_n;
+  output [16:0]ddr4_sdram_adr;
+  output [1:0]ddr4_sdram_ba;
+  output ddr4_sdram_bg;
+  output ddr4_sdram_ck_c;
+  output ddr4_sdram_ck_t;
+  output ddr4_sdram_cke;
+  output ddr4_sdram_cs_n;
+  inout [7:0]ddr4_sdram_dm_n;
+  inout [63:0]ddr4_sdram_dq;
+  inout [7:0]ddr4_sdram_dqs_c;
+  inout [7:0]ddr4_sdram_dqs_t;
+  output ddr4_sdram_odt;
+  output ddr4_sdram_reset_n;
+  input default_sysclk_300_clk_n;
+  input default_sysclk_300_clk_p;
+  input [7:0]pci_express_x8_rxn;
+  input [7:0]pci_express_x8_rxp;
+  output [7:0]pci_express_x8_txn;
+  output [7:0]pci_express_x8_txp;
+  input pcie_perstn;
+  input pcie_refclk_clk_n;
+  input pcie_refclk_clk_p;
+  input reset;
+
+  wire ddr4_sdram_act_n;
+  wire [16:0]ddr4_sdram_adr;
+  wire [1:0]ddr4_sdram_ba;
+  wire ddr4_sdram_bg;
+  wire ddr4_sdram_ck_c;
+  wire ddr4_sdram_ck_t;
+  wire ddr4_sdram_cke;
+  wire ddr4_sdram_cs_n;
+  wire [7:0]ddr4_sdram_dm_n;
+  wire [63:0]ddr4_sdram_dq;
+  wire [7:0]ddr4_sdram_dqs_c;
+  wire [7:0]ddr4_sdram_dqs_t;
+  wire ddr4_sdram_odt;
+  wire ddr4_sdram_reset_n;
+  wire default_sysclk_300_clk_n;
+  wire default_sysclk_300_clk_p;
+  wire [7:0]pci_express_x8_rxn;
+  wire [7:0]pci_express_x8_rxp;
+  wire [7:0]pci_express_x8_txn;
+  wire [7:0]pci_express_x8_txp;
+  wire pcie_perstn;
+  wire pcie_refclk_clk_n;
+  wire pcie_refclk_clk_p;
+  wire reset;
+
+  design_1 design_1_i
+       (.ddr4_sdram_act_n(ddr4_sdram_act_n),
+        .ddr4_sdram_adr(ddr4_sdram_adr),
+        .ddr4_sdram_ba(ddr4_sdram_ba),
+        .ddr4_sdram_bg(ddr4_sdram_bg),
+        .ddr4_sdram_ck_c(ddr4_sdram_ck_c),
+        .ddr4_sdram_ck_t(ddr4_sdram_ck_t),
+        .ddr4_sdram_cke(ddr4_sdram_cke),
+        .ddr4_sdram_cs_n(ddr4_sdram_cs_n),
+        .ddr4_sdram_dm_n(ddr4_sdram_dm_n),
+        .ddr4_sdram_dq(ddr4_sdram_dq),
+        .ddr4_sdram_dqs_c(ddr4_sdram_dqs_c),
+        .ddr4_sdram_dqs_t(ddr4_sdram_dqs_t),
+        .ddr4_sdram_odt(ddr4_sdram_odt),
+        .ddr4_sdram_reset_n(ddr4_sdram_reset_n),
+        .default_sysclk_300_clk_n(default_sysclk_300_clk_n),
+        .default_sysclk_300_clk_p(default_sysclk_300_clk_p),
+        .pci_express_x8_rxn(pci_express_x8_rxn),
+        .pci_express_x8_rxp(pci_express_x8_rxp),
+        .pci_express_x8_txn(pci_express_x8_txn),
+        .pci_express_x8_txp(pci_express_x8_txp),
+        .pcie_perstn(pcie_perstn),
+        .pcie_refclk_clk_n(pcie_refclk_clk_n),
+        .pcie_refclk_clk_p(pcie_refclk_clk_p),
+        .reset(reset));
+endmodule
---
+++ vivado_designs/DMA_2/DMA_2.srcs/sources_1/bd/design_1/ip/design_1_util_ds_buf_0/design_1_util_ds_buf_0_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (lin64) Build 2700185 Thu Oct 24 18:45:48 MDT 2019
+-- Date        : Mon Jan  6 15:48:36 2020
+-- Host        : desktop running 64-bit Ubuntu 18.04.2 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/luke/Documents/Capstone/FPGA_API/vivado_designs/DMA_2/DMA_2.srcs/sources_1/bd/design_1/ip/design_1_util_ds_buf_0/design_1_util_ds_buf_0_stub.vhdl
+-- Design      : design_1_util_ds_buf_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xcku040-ffva1156-2-e
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_util_ds_buf_0 is
+  Port ( 
+    IBUF_DS_P : in STD_LOGIC_VECTOR ( 0 to 0 );
+    IBUF_DS_N : in STD_LOGIC_VECTOR ( 0 to 0 );
+    IBUF_OUT : out STD_LOGIC_VECTOR ( 0 to 0 );
+    IBUF_DS_ODIV2 : out STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end design_1_util_ds_buf_0;
+
+architecture stub of design_1_util_ds_buf_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "IBUF_DS_P[0:0],IBUF_DS_N[0:0],IBUF_OUT[0:0],IBUF_DS_ODIV2[0:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "util_ds_buf,Vivado 2019.2";
+begin
+end;
