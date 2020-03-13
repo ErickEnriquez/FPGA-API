02457fab127e76dfebdf01d3f2386015aad52af0
erick <eenriqu5@asu.edu>
2020-03-13 10:36:03 -0700

    Update parser.cpp
    started to work on forking to create a new proccess and then using execvp to change the child proccess to use the driver provided by xillinx

--- parser.cpp
+++ parser.cpp
@@ -1,21 +1,27 @@
 #ifdef __WIN32
-    #include <Windows.h>               //strncpy
+#include <Windows.h> //strncpy
 #elif __linux__
-    //any linux specific libraries
+//any linux specific libraries
+#include <unistd.h>    // fork()
+#include <sys/types.h> // for pid_t
+#include <sys/wait.h>  //for wait()
+#include <errno.h>     //printing errors
 #endif
 
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <fstream>
-#include <algorithm>    // std::transform
-
+#include <algorithm> // std::transform
 
 #define DDR4 0x80000000                // Base DMA
 #define UART_BASE 0x44A01000           // BaseUART
 #define UART_STATUS_CONTROL 0x44A02000 //
 #define ETHERNET 0x00000000            //placeholder
 #define USB 0x00000000                 //placeholder
+
+#define DMA_TO_DEVICE_EXE "/Linux-PCIe-DMA-Driver/tools/dma_to_device"
+#define DMA_FROM_DEVICE_EXE "/Linux-PCIe-DMA-Driver/tools/dma_from_device"
 
 /*What wae are going to need in order to make a successful host to card or card to host transfer transfer is 
 -d DEVICE
@@ -29,10 +35,10 @@ using namespace std;
 
 void parser(int, string *);
 void dma_to_device(char *fileName, unsigned long Address);
-void dma_from_device(char *fileName , unsigned long Address);
+void dma_from_device(char *fileName, unsigned long Address);
 void deleter(char *fileName);
 int string_splitter(string, string *);
-bool string_case_compare(string , string);
+bool string_case_compare(string, string);
 unsigned long check_address(string);
 void print_error();
 void helper();
@@ -40,7 +46,7 @@ void helper();
 int main(int argc, char **argv)
 {
     string running_input;
-    string *input_buffer = new string[3];//buffer to hold user commands
+    string *input_buffer = new string[3]; //buffer to hold user commands
     for (int i = 1; i < argc; i++)
     {
         input_buffer[i - 1] = argv[i];
@@ -54,30 +60,30 @@ int main(int argc, char **argv)
             helper();                                        //print the info
         getline(cin, running_input, '\n');                   //get user input
         argc = string_splitter(running_input, input_buffer); //split the input
-    } while (string_case_compare("Quit",input_buffer[0]) == false);
+    } while (string_case_compare("Quit", input_buffer[0]) == false);
     return 0;
 }
 
 void parser(int argc, string *argv)
 {
-    if (string_case_compare("help",argv[0]) == true)
+    if (string_case_compare("help", argv[0]) == true)
         helper();
-    else if (string_case_compare("delete",argv[0]) == true)
+    else if (string_case_compare("delete", argv[0]) == true)
     {
         char *file_name = new char[argv[1].size() + 1];
-        strncpy(file_name, argv[1].c_str(), argv[1].size());//copy the string to a Cstring
+        strncpy(file_name, argv[1].c_str(), argv[1].size()); //copy the string to a Cstring
         deleter(file_name);
     }
-    else if (string_case_compare("dma_to_device",argv[0]) == true)
+    else if (string_case_compare("dma_to_device", argv[0]) == true)
     {
         if (argv[1].empty() == false)
         {
             if (argv[2].empty() == false)
             { //if we have a valid dma from host to card transfer
                 unsigned long Address = check_address(argv[2]);
-                char* filename = new char[argv[1].size() + 1];
-                strncpy(filename, argv[1].c_str(), argv[1].size());//copy the string to a Cstring
-                dma_to_device(filename,Address);
+                char *filename = new char[argv[1].size() + 1];
+                strncpy(filename, argv[1].c_str(), argv[1].size()); //copy the string to a Cstring
+                dma_to_device(filename, Address);
             }
             else
                 print_error();
@@ -85,16 +91,16 @@ void parser(int argc, string *argv)
         else
             print_error();
     }
-    else if (string_case_compare("dma_from_device",argv[0]) == true)
+    else if (string_case_compare("dma_from_device", argv[0]) == true)
     {
         if (argv[1].empty() == false)
         {
             if (argv[2].empty() == false)
             { //if we have a valid dma from card to host transfer
-              unsigned long Address = check_address(argv[2]);
-                char* filename = new char[argv[1].size() + 1];
-                strncpy(filename, argv[1].c_str(), argv[1].size());//copy the string to a Cstring
-                dma_from_device(filename,Address);
+                unsigned long Address = check_address(argv[2]);
+                char *filename = new char[argv[1].size() + 1];
+                strncpy(filename, argv[1].c_str(), argv[1].size()); //copy the string to a Cstring
+                dma_from_device(filename, Address);
             }
             else
                 print_error();
@@ -116,14 +122,97 @@ void dma_to_device(char *fileName, unsigned long Address)
     { // if file does not exist
         cout << "ERROR OPENING FILE\n";
         helper();
-        exit(0);
     }
     else
     {
+        fseek(transferFile, 0, SEEK_END);   //go the the end of the file
+        int filesize = ftell(transferFile); //get the size of the file
+        fseek(transferFile, 0, SEEK_SET);   //return to the top of the file in order to ensure we read correctly
+        fclose(transferFile);
+#ifdef __WIN32
+        //use create_proccess() to call the driver
+
+#elif __linux__
+
+        pid_t pid; //proccess id
+        int ret = 1;
+        int status;
+
+        pid = fork();
+
+        if (pid == -1)
+        { //error occured
+            printf("Error occured , couldn't fork\n");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            printf("child proccess : %u started", getpid());
+            // -d DEVICE
+            // -f FILE TO BE TRANSFERED
+            // -s SIZE OF THE FILE
+            // -a ADDRESS OFFSET
+            // -c TRANSFER COUNT
+            char* size, addr;
+            addr = (char*)malloc(8);//store the 8 bytes
+            sprintf(addr,"%d",Address);
+            char* args[] = {"-d ", " -f ", fileName, " -s ", " -a ", , " -c ", NULL};
+            execvp(DMA_TO_DEVICE_EXE,args);
+        }
+        else
+        {
+            //we get a positive number which corresponds to the pid of the parent proccess
+
+            printf("parent process, pid = %u\n", getppid());
+
+            // the parent process calls waitpid() on the child
+            // waitpid() system call suspends execution of
+            // calling process until a child specified by pid
+            // argument has changed state
+            // see wait() man page for all the flags or options
+            // used here
+
+            if (waitpid(pid, &status, 0) > 0)
+            {
+
+                if (WIFEXITED(status) && !WEXITSTATUS(status))
+
+                    printf("program execution successful\n");
+
+                else if (WIFEXITED(status) && WEXITSTATUS(status))
+                {
+
+                    if (WEXITSTATUS(status) == 127)
+                    {
+
+                        // execv failed
+
+                        printf("execv failed\n");
+                    }
+
+                    else
+
+                        printf("program terminated normally, but returned a non-zero status\n");
+                }
+
+                else
+
+                    printf("program didn't terminate normally\n");
+            }
+
+            else
+            {
+                // waitpid() failed
+                printf("waitpid() failed\n");
+            }
+            exit(0);
+        }
+
+#endif
     }
 }
 
-void dma_from_device(char *fileName , unsigned long Address)
+void dma_from_device(char *fileName, unsigned long Address)
 {
     //Code for finding the file in the FPGA & retrieving goes here
     FILE *fileData;
@@ -189,26 +278,28 @@ void print_error()
 }
 
 //helper function doing case insensitive comparision
-bool string_case_compare(string s1, string s2){
-	transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+bool string_case_compare(string s1, string s2)
+{
+    transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
     transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
-    if( s1 == s2)
+    if (s1 == s2)
         return true;
     else
         return false;
 }
 
-unsigned long check_address(string s1){
-    if(string_case_compare(s1,"DDR4") == true)
+unsigned long check_address(string s1)
+{
+    if (string_case_compare(s1, "DDR4") == true)
         return DDR4;
-    else if(string_case_compare(s1,"UART_BASE") == true)
+    else if (string_case_compare(s1, "UART_BASE") == true)
         return UART_BASE;
-    else if(string_case_compare(s1,"UART_STATUS_CONSTROL") == true)
+    else if (string_case_compare(s1, "UART_STATUS_CONSTROL") == true)
         return UART_STATUS_CONTROL;
-    else if(string_case_compare(s1,"ETHERNET") == true)
+    else if (string_case_compare(s1, "ETHERNET") == true)
         return ETHERNET;
-    else if(string_case_compare(s1,"USB") == true)
+    else if (string_case_compare(s1, "USB") == true)
         return USB;
     else
-        return stoul(s1,nullptr,10);//convert the string to an unsigne long
+        return stoul(s1, nullptr, 10); //convert the string to an unsigne long
 }
