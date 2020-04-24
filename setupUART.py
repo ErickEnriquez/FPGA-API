3b6d4b4a63469fa34a96db7c52b8b0bfe4073a7e
Erick Enriquez <eenriqu5@asu.edu>
2020-04-23 20:14:19 -0700

    back to fix on my machine

--- setupUART.py
+++ setupUART.py
@@ -5,14 +5,26 @@ WindowsPathToXdma = 'C:/Users/butler/Documents/GitHub/FPGA_API/Xilinx_Answer_654
 LinuxPathToXdma = 'Linux-PCIe-DMA-Driver/XDMA/linux-kernel/tools/dma_to_device '
 
 
-uart_base_address = 0x44A01000
-
-
-
-setup_write = ' 0x00 ' +'0x00 '+'0x00 '+'0x00 '+'0x03 '+'0x00 '+'0x00 '+'0x00 '+'0xc7 '+'0x00 '+'0x00 '+'0x00 '+'0x1b '+'0x00 '+'0x00 '+'0x00'
 
 def setup_uart():
+
+    uart_base_address = 0x44A01000
+
+    uart_line_control =  0x440a100c
+
+    write1 = ' 0x00 ' + '0x00 ' + '0x00 ' + '0x00 '
+    write2 = '0x00 ' + '0x00 ' + '0x00 ' + '0x03 '
+    write3 = '0x00 '  + '0x00 ' + '0x00 ' + '0xC7 '
+    write4 = '0x00 '  + '0x00 ' + '0x00 ' + '0x1B '
+
+    total_write = write1  +write2 + write3 + write4
+    
+
     if sys.platform == 'win32':
-     args = WindowsPathToXdma + 'h2c_1 ' + 'write '  + str(uart_base_address) + setup_write
-     print(args)
+        #args = WindowsPathToXdma + 'h2c_1 ' + 'write '  + str(uart_base_address) + total_write
+        args = WindowsPathToXdma + 'h2c_1 ' + 'write '  + str(uart_line_control) + " " + write4
+        p1 = subprocess.Popen(args)
+        p1.wait()
+        print('WRITE ' , write4 , ' to location ' , uart_line_control)
+     
 
